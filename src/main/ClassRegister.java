65497df582bc6f48784c747e1dc1702d3624f37c
Sin <sinlord@icloud.com>
2024-10-17 17:14:32 +0200

    优化了程序入口，目前发现BD断连会重置标题数据来源为null

--- src/main/ClassRegister.java
+++ src/main/ClassRegister.java
@@ -7,10 +7,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ClassRegister {
-    public static Map<String, Class<? extends GeneralControl<?>>> classMap = new HashMap<>();
+    public static Map<String, Class<? extends GeneralControl>> classMap = new HashMap<>();
     public static Map<String,Class<? extends GeneralDataAccess>> DAMap = new HashMap<>();
 
-    public static void registerControl(String className, Class<? extends GeneralControl<?>> control_Class) {
+    public static void registerControl(String className, Class<? extends GeneralControl> control_Class) {
         classMap.put(className, control_Class);
     }
 
@@ -22,8 +22,8 @@ public class ClassRegister {
         return classMap.keySet().toArray(new String[0]);
     }
 
-    public static GeneralControl<?> getControl(String class_name) throws Exception {
-        Class<? extends GeneralControl<?>> controlClass = classMap.get(class_name);
+    public static GeneralControl getControl(String class_name) throws Exception {
+        Class<? extends GeneralControl> controlClass = classMap.get(class_name);
         if (controlClass != null) {
             return controlClass.getDeclaredConstructor().newInstance();
         } else {
--- src/main/principal.java
+++ src/main/principal.java
@@ -11,14 +11,26 @@ import static GUI.GeneralMenu.buildSelectionDialog;
 
 public class principal {
 
-    public static void main(String[] args) {
-        initialize();
+    public static void main(String[] args) throws Exception {
+        class_register();
+        GeneralControl current_control = control_chooser();
+        current_control.run();
+    }
+
+    public static void class_register(){
+        ClassRegister.registerControl("Player", PlayerControl.class);
+        ClassRegister.registerDataAccess("Player", PlayerDataAccess.class);
+        ClassRegister.registerControl("Not available", null);
+        ClassRegister.registerDataAccess("Not available", null);
+    }
+
+    public static GeneralControl control_chooser(){
         while(true){
             try{
                 String chosen_Class = buildSelectionDialog("Data Class Menu","Choose a data class", ClassRegister.getClassNames());
                 GeneralControl current_control = ClassRegister.getControl(chosen_Class);
                 current_control.setDA(ClassRegister.getDA(chosen_Class));
-                current_control.run();
+                return current_control;
             }catch (OperationCanceledException e){
                 System.exit(0);
             }catch (Exception e) {
@@ -26,12 +38,4 @@ public class principal {
             }
         }
     }
-
-    public static void initialize(){
-        ClassRegister.registerControl("Player", PlayerControl.class);
-        ClassRegister.registerDataAccess("Player", PlayerDataAccess.class);
-        ClassRegister.registerControl("Not available", null);
-        ClassRegister.registerDataAccess("Not available", null);
-    }
-
 }
