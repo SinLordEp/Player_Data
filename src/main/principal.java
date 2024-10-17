f37f60d4c6fa25a11b4572c25dfc037eb2548c72
Sin <sinlord@icloud.com>
2024-10-17 15:43:34 +0200

    新增对多种SQL支持的接口，新增SQLite的支持，优化了标题对数据来源更新的逻辑以及表格的清楚数据逻辑

--- src/main/principal.java
+++ src/main/principal.java
@@ -3,7 +3,6 @@ package main;
 import GUI.GeneralMenu;
 import control.GeneralControl;
 import control.PlayerControl;
-import data.GeneralDataAccess;
 import data.PlayerDataAccess;
 
 import static GUI.GeneralMenu.buildSelectionDialog;
@@ -17,7 +16,7 @@ public class principal {
         while(true){
             try{
                 String chosen_Class = buildSelectionDialog("Data Class Menu","Choose a data class", ClassRegister.getClassNames());
-                GeneralControl<? extends GeneralDataAccess> current_control = ClassRegister.getControl(chosen_Class);
+                GeneralControl current_control = ClassRegister.getControl(chosen_Class);
                 current_control.setDA(ClassRegister.getDA(chosen_Class));
                 current_control.run();
             }catch (OperationCanceledException e){
