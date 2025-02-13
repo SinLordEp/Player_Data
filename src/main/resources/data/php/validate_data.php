421e0783fbf3459f21b7a47596ab2222ac112c8d
Sin <sinlord@icloud.com>
2025-02-13 20:17:39 +0100

    change: change the data sent to PHP from array to single entity

--- src/main/resources/data/php/validate_data.php
+++ src/main/resources/data/php/validate_data.php
@@ -4,15 +4,15 @@ function validate($expectedFormat, array $data):bool
     if(empty($data)){
         return false;
     }
-    foreach ($data as $value) {
-        foreach ($expectedFormat as $expectedKey => $expectedValue){
-            if(!array_key_exists($expectedKey, $value)){
-                return false;
-            }
-            if(gettype($value[$expectedKey]) !== gettype($expectedValue)){
-                return false;
-            }
+
+    foreach ($expectedFormat as $expectedKey => $expectedValue){
+        if(!array_key_exists($expectedKey, $data)){
+            return false;
+        }
+        if(gettype($data[$expectedKey]) !== gettype($expectedValue)){
+            return false;
         }
     }
+
     return true;
 }
--- src/main/resources/data/php/write_player.php
+++ src/main/resources/data/php/write_player.php
@@ -30,12 +30,6 @@ if (json_last_error() !== JSON_ERROR_NONE) {
     return $finalMessage;
 }
 
-if (!is_array($decodedPlayer)) {
-    file_put_contents("debug.log", "Decoded Data is not an array: " . print_r($decodedPlayer, true) . PHP_EOL, FILE_APPEND);
-    $finalMessage["status"] = "error";
-    $finalMessage["message"] = "Expected JSON array, received something else";
-    return $finalMessage;
-}
 
 if(!validate($expectedFormat, $decodedPlayer)){
     $finalMessage["status"] = "error";
@@ -44,56 +38,56 @@ if(!validate($expectedFormat, $decodedPlayer)){
 }
 
 $conn->begin_transaction();
-foreach ($decodedPlayer as $player) {
-    //$query = mysqli_stmt::class;
-    $id = $player["id"];
-    $name = $player["name"];
-    $region = $player["region"];
-    $server = $player["server"];
-    switch($player["operation"]){
-        case "ADD":
-            $query = $conn->prepare("INSERT INTO player (id, name, region, server) values (?,?,?,?)");
-            $query->bind_param("isss",$id,$name, $region, $server);
-            break;
-        case "MODIFY":
-            $query = $conn->prepare("UPDATE player SET name = ?, region = ?, server = ? WHERE id = ?");
-            $query->bind_param("sssi", $id, $region, $server, $id);
-            break;
-        case "DELETE":
-            $query = $conn->prepare("DELETE FROM player WHERE id = ?");
-            $query->bind_param("i", $id);
-            break;
-        default:
-            $finalMessage["status"] = "error";
-            $finalMessage["message"] = "Unexpected Operation Code received!";
-            finish($finalMessage);
-            break;
-    }
-    $query->execute();
-    switch($query->affected_rows){
-        case -1:
-            $finalMessage["status"] = "error";
-            $finalMessage["message"] = "Failed to modify player data with ID: $id with cause: $conn->error";
-            $query->close();
-            finish($finalMessage);
-            break;
-        case 0:
-            $finalMessage["status"] = "error";
-            $finalMessage["message"] = "Update request eas executed but no player data with $id was affected!";
-            $query->close();
-            finish($finalMessage);
-            break;
-        case 1:
-            break;
-        default:
-            $finalMessage["status"] = "error";
-            $finalMessage["message"] = "Update request was executed but more than one row were affected!";
-            $query->close();
-            finish($finalMessage);
-            break;
-    }
-    $query->close();
+
+//$query = mysqli_stmt::class;
+$id = $decodedPlayer["id"];
+$name = $decodedPlayer["name"];
+$region = $decodedPlayer["region"];
+$server = $decodedPlayer["server"];
+switch($decodedPlayer["operation"]){
+    case "ADD":
+        $query = $conn->prepare("INSERT INTO player (id, name, region, server) values (?,?,?,?)");
+        $query->bind_param("isss",$id,$name, $region, $server);
+        break;
+    case "MODIFY":
+        $query = $conn->prepare("UPDATE player SET name = ?, region = ?, server = ? WHERE id = ?");
+        $query->bind_param("sssi", $id, $region, $server, $id);
+        break;
+    case "DELETE":
+        $query = $conn->prepare("DELETE FROM player WHERE id = ?");
+        $query->bind_param("i", $id);
+        break;
+    default:
+        $finalMessage["status"] = "error";
+        $finalMessage["message"] = "Unexpected Operation Code received!";
+        finish($finalMessage);
+        break;
 }
+$query->execute();
+switch($query->affected_rows){
+    case -1:
+        $finalMessage["status"] = "error";
+        $finalMessage["message"] = "Failed to modify player data with ID: $id with cause: $conn->error";
+        $query->close();
+        finish($finalMessage);
+        break;
+    case 0:
+        $finalMessage["status"] = "error";
+        $finalMessage["message"] = "Update request eas executed but no player data with $id was affected!";
+        $query->close();
+        finish($finalMessage);
+        break;
+    case 1:
+        break;
+    default:
+        $finalMessage["status"] = "error";
+        $finalMessage["message"] = "Update request was executed but more than one row were affected!";
+        $query->close();
+        finish($finalMessage);
+        break;
+}
+$query->close();
+
 $conn->commit();
 $finalMessage["status"] = "success";
 finish($finalMessage);
