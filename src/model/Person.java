e4eb7a798fb9b91f4fd2be1552304d84f680f461
Sin <sinlord@icloud.com>
2025-02-20 16:17:56 +0100

    change: removed hibernate @ auto generate ID

--- src/model/Person.java
+++ src/model/Person.java
@@ -13,7 +13,6 @@ public abstract class Person implements java.io.Serializable, VerifiedEntity {
     @javax.persistence.Id
     @javax.persistence.GeneratedValue
     @Id
-    @GeneratedValue
     @Column(name = "id")
     protected int ID = 0;
 
