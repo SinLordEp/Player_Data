ed50b47767035e5c7b340e51d58b722d163b387f
Sin <sinlord@icloud.com>
2025-02-26 17:08:14 +0100

    polish: polished code

--- src/data/CRUDFactory.java
+++ src/data/CRUDFactory.java
@@ -19,11 +19,19 @@ public class CRUDFactory {
     private final static HashMap<String, Function<DataInfo, GeneralCRUD<DataInfo>>> CRUD_INSTANCES = new HashMap<>();
 
     static {
+        initializeCRUD();
+    }
+
+    @SuppressWarnings("unchecked")
+    private static void initializeCRUD() {
         List<String> supportedCRUD = Arrays.asList(getProperty("supportedCRUD").split(","));
         supportedCRUD.forEach(dataType -> CRUD_INSTANCES.put(dataType, (dataInfo) ->{
             try {
-                Class<GeneralCRUD<DataInfo>> tempClass = (Class<GeneralCRUD<DataInfo>>) Class.forName("data.%s".formatted(getProperty(dataType)));
-                return tempClass.getDeclaredConstructor(DataInfo.class).newInstance(dataInfo);
+                Class<?> tempClass = Class.forName("data.%s".formatted(getProperty(dataType)));
+                if(GeneralCRUD.class.isAssignableFrom(tempClass)){
+                    return (GeneralCRUD<DataInfo>) tempClass.getDeclaredConstructor(DataInfo.class).newInstance(dataInfo);
+                }
+                throw new OperationException("Unsupported CRUD type: " + dataType);
             } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | InstantiationException |
                      IllegalAccessException e) {
                 throw new OperationException("%sCRUD could not be instantiated".formatted(dataType));
