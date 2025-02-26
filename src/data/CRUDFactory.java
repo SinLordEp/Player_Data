d95e57087025cdc7632e9fe4ab58fd33f7f8e4ad
Sin <sinlord@icloud.com>
2025-02-26 16:07:54 +0100

    change: refactored CRUDFactory, now it storages CRUD newInstance function in hashmap to avoid using reflection everytime a CRUD is called.

--- src/data/CRUDFactory.java
+++ src/data/CRUDFactory.java
@@ -5,6 +5,10 @@ import exceptions.OperationException;
 import model.DataInfo;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.Function;
 
 import static main.principal.getProperty;
 
@@ -12,15 +16,27 @@ import static main.principal.getProperty;
  * @author SIN
  */
 public class CRUDFactory {
-    @SuppressWarnings("unchecked")
-    public static GeneralCRUD<DataInfo> getCRUD(DataInfo dataInfo) {
-        String classPackagePath = "data.%s".formatted(getProperty(dataInfo.getDataType().toString()));
-        try {
-            Class<GeneralCRUD<DataInfo>> tempClass = (Class<GeneralCRUD<DataInfo>>) Class.forName(classPackagePath);
-            return tempClass.getDeclaredConstructor(DataInfo.class).newInstance(dataInfo);
-        } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | InstantiationException |
-                 IllegalAccessException e) {
-            throw new OperationException("%sCRUD could not be instantiated".formatted(dataInfo.getDataType()));
-        }
+    private final static HashMap<String, Function<DataInfo, GeneralCRUD<DataInfo>>> CRUD_INSTANCES = new HashMap<>();
+
+    static {
+        List<String> supportedCRUD = Arrays.asList(getProperty("supportedCRUD").split(","));
+        supportedCRUD.forEach(dataType -> {
+            CRUD_INSTANCES.put(dataType, (dataInfo) ->{
+                try {
+                    Class<GeneralCRUD<DataInfo>> tempClass = (Class<GeneralCRUD<DataInfo>>) Class.forName("data.%s".formatted(getProperty(dataType)));
+                    return tempClass.getDeclaredConstructor(DataInfo.class).newInstance(dataInfo);
+                } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | InstantiationException |
+                         IllegalAccessException e) {
+                    throw new OperationException("%sCRUD could not be instantiated".formatted(dataType));
+                }
+            });
+
+        });
     }
+
+    public static GeneralCRUD<DataInfo> getCRUD(DataInfo dataInfo) {
+        return CRUD_INSTANCES.get(dataInfo.getDataType().toString()).apply(dataInfo);
+    }
+
+
 }
